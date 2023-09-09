ae8fa1a6da75caf2f90bb3f88711ac64601ca729
eunho <jeh4916@gmail.com>
2023-09-09 11:55:07 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const loginButton = document.querySelector("#login-form button");
 function onLoginBtnClick() {
   // console.log("click!!!");
   // console.dir(loginInput);
-  console.log("hello", loginInput.value);
+  // console.log("hello", loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
